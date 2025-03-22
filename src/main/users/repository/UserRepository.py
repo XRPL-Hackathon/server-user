777ec581ea058446c8433d78c1cb88c234b0815b
seyeon22 <sywolleych@gmail.com>
2025-03-23 03:34:01 +0900

    Refactor: 지갑 생성 기능에서 seed 저장 추가

--- src/main/users/repository/UserRepository.py
+++ src/main/users/repository/UserRepository.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from src.main.config.mongodb import get_mongo_client
-from src.main.nft.nft_user_info import User
+# from src.main.nft.nft_user_info import User
 
 class UserRepository:
     def __init__(self):
@@ -12,33 +12,31 @@ class UserRepository:
         wallets = list(self.wallets_collection.find({"user_id": user_id}, {"_id": 0}))
         return wallets if wallets else []
     
-    def save_wallet(self, user_id: str, wallet_address: str, point: int = 0, nft_grade: str = "bronze"):
+    def save_wallet(self, user_id: str, wallet_address: str, seed: str, point: int = 0, nft_grade: str = "bronze"):
+        wallet_data ={
+            "address": wallet_address,
+            "seed": seed,
+            "point": point,
+            "nft_grade": nft_grade
+        }
         existing = self.wallets_collection.find_one({"user_id": user_id})
         if existing:
             self.wallets_collection.update_one(
                 {"user_id": user_id}, 
-                {"$set": {"address": wallet_address,
-                          "point": point,
-                          "nft_grade": nft_grade}}
+                {"$set": wallet_data}
             )
             return {"message": "Wallet updated", 
                     "user_id": user_id, 
-                    "wallet_address": wallet_address,
-                    "point": point,
-                    "nft_grade": nft_grade}
+                    **wallet_data}
         else:
             self.wallets_collection.insert_one({"user_id": user_id, 
-                                                "address": wallet_address,
-                                                "point": point,
-                                                "nft_grade": nft_grade})
+                                                **wallet_data})
             return {"message": "Wallet created", 
                     "user_id": user_id, 
-                    "wallet_address": wallet_address,
-                    "point": point,
-                    "nft_grade": nft_grade}
+                    **wallet_data}
         
     
-    def get_all_user(self) -> list[User]:
-        docs = self.wallets_collection.find({}, {"_id": 0, "address":1, "point": 1})
-        users = [User(wallet=doc["address"], point=doc.get("point", 0)) for doc in docs]
-        return users
+    # def get_all_user(self) -> list[User]:
+    #     docs = self.wallets_collection.find({}, {"_id": 0, "address":1, "point": 1})
+    #     users = [User(wallet=doc["address"], point=doc.get("point", 0)) for doc in docs]
+    #     return users
